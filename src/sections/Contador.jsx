4337e3deacbe5c0ec915255c6e2c7e36c5101e23
VictorGarcia2 <victrgarciaphoto@gmail.com>
2025-02-13 07:11:43 -0600

    Actualizar nombres en el componente Contador

--- src/sections/Contador.jsx
+++ src/sections/Contador.jsx
@@ -8,7 +8,7 @@ export default function Contador() {
       <div className="flex flex-col items-center  ">
         <h1 className=" text-white mt-20 text-6xl absolute font-display">
           {" "}
-          Victor & <br /> Marilyn
+          Alex & <br /> Agata
         </h1>
         <div className="absolute  mt-[500px]  w-full ">
           <p className="w-full font-display text-center text-xl tracking-widest text-white">21 DE OCTUBRE 2025</p>
