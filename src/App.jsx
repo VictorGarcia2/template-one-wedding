eff69d532a11e10a485b641b789b1862700c5d39
VictorGarcia2 <victrgarciaphoto@gmail.com>
2025-02-15 16:42:45 -0600

    Renombrar el componente de asistencia a RSVPList y mover la lógica de estado a un nuevo archivo para mejorar la organización del código.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 import { Route, Routes } from "react-router";
 import HomePage from "./sections/pages/HomePage";
-import RSVPList from "./sections/pages/asistencia";
+import RSVPList from "./sections/pages/RSVPList ";
 export default function App() {
 
   return (
