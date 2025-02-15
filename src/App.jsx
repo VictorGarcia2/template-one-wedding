6b5bb4bd3badc183017798bd811b58f9471c35b3
VictorGarcia2 <victrgarciaphoto@gmail.com>
2025-02-15 16:37:25 -0600

    Eliminar importaciones no utilizadas en App.jsx y agregar archivo de redirección para mejorar la gestión de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,6 @@
 import React from "react";
-import Contador from "./sections/Contador";
-import Padres from "./sections/Padres";
-import Dedicatoria from "./sections/Dedicatoria";
-import Itinerario from "./sections/Itinerario";
-import Dresscode from "./sections/Dresscode";
-import MesaDeRegalos from "./sections/MesaDeRegalos";
-import CountdownTimer from "./components/CountdownTimer";
-import Footer from "./sections/Footer";
-import Asistencia from "./sections/Asistencia";
-import Amenidades from "./sections/Amenidades";
-import Galeria from "./sections/Galeria";
-import Hospedaje from "./sections/Hospedaje";
-import Noninos from "./sections/Noninos";
+
+
 import { Route, Routes } from "react-router";
 import HomePage from "./sections/pages/HomePage";
 import RSVPList from "./sections/pages/asistencia";
