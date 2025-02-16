4871ff9bcf6645fe1f24e5a42bb9a722670008a2
VictorGarcia2 <victrgarciaphoto@gmail.com>
2025-02-16 15:06:21 -0600

    Eliminar el restablecimiento de campos en la función de alerta en Asistencia

--- src/sections/Asistencia.jsx
+++ src/sections/Asistencia.jsx
@@ -34,8 +34,6 @@ export default function () {
   const alert = ()=>{
     if (name !== "" && asistencia !== "") {
       setAlerts(true)
-      setName("")
-      setAsistencia("")
     }
   }
   return (
