33c1de0be76521b158e9d55014cd23ce8c4370b2
VictorGarcia2 <victrgarciaphoto@gmail.com>
2025-02-17 22:39:37 -0600

    Actualizar el estilo del componente AlertAsistencia, cambiando el fondo a blanco

--- src/components/AlertAsistencia.jsx
+++ src/components/AlertAsistencia.jsx
@@ -3,8 +3,8 @@ import React from "react";
 import animationData from "../assets/Success.json" 
 export default function AlertAsistencia({alerts}) {
   return (
-    <div className={`${alerts ? "animate-fade animate-once animate-duration-[3200] animate-delay-100 animate-ease-in" : "hidden"} flex z-50 h-full  fixed top-0 items-center justify-center bg-gray-100`}>
-      <div className="rounded-lg bg-gray-50 px-16 py-14">
+    <div className={`${alerts ? "animate-fade animate-once animate-duration-[3200] animate-delay-100 animate-ease-in" : "hidden"} flex z-50 h-full  fixed top-0 items-center justify-center bg-white`}>
+      <div className="rounded-lg bg-white px-16 py-14">
         <div className="flex justify-center">
           <div className="">
             <div className="flex h-full w-full items-center justify-center">
